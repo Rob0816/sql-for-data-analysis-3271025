0ed7793bcdde6cc4965aebe561d3f7e578918a74
Nikiya Simpson <nikiyasimpson@gmail.com>
2026-01-27 08:16:30 -0600

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -6,5 +6,5 @@ fi
 
 if [ -f .devcontainer/H_Plus_Sports_MySQL.sql ]; then
   sleep 20
-  mysql -h 127.0.0.1 -umariadb -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql
+  mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql
 fi

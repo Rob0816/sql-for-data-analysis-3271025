bb262c153f655a44e741d4ffccacb7a50a77fd45
Rob0816 <robert.wandt@bmw.de>
2026-02-18 07:28:06 +0000

    findingOrders

---
+++ Chapter Rob/04_findFaltyPricing.sql
@@ -0,0 +1,10 @@
+SELECT OrderItem.OrderId,
+  Orders.TotalDue,
+  SUM(Price * Quantity) as NewTotalDue
+FROM OrderItem
+  JOIN Product ON OrderItem.ProductID = Product.ProductID
+  JOIN Orders ON OrderItem.OrderID = Orders.OrderID
+GROUP BY OrderItem.OrderId,
+  Orders.TotalDue
+HAVING Orders.TotalDue <> SUM(Price * Quantity)
+ORDER BY Orders.TotalDue DESC;
